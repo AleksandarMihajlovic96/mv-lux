570dfc53cf3d9755aed76c6a171d5e38b9f84def
Aleksandar <aleksandar.mihajlovic96@yahoo.com>
2023-04-06 00:45:53 +0200

    Base ProductsNav design

--- src/components/products-nav/ProductsNav.tsx
+++ src/components/products-nav/ProductsNav.tsx
@@ -1,11 +1,59 @@
+import { Tab, Tabs } from "@mui/material";
+import { useState } from "react";
 import styled from "styled-components";
 
 const ProductsNavContainer = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 16px;
 `;
 
+const ProductsNavTab = styled(Tab)({
+  "&&": {
+    color: "black",
+    fontWeight: "bold",
+    "&:hover": {
+      background: "#e34234",
+      color: "white",
+    },
+  },
+});
+
 const ProductsNav = () => {
-  return <ProductsNavContainer></ProductsNavContainer>;
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  return (
+    <ProductsNavContainer>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+        TabIndicatorProps={{
+          style: {
+            background: "#e34234",
+            color: "white"
+          },
+        }}
+      >
+        <ProductsNavTab label="Svi proizvodi" />
+        <ProductsNavTab label="Klasični lusteri" />
+        <ProductsNavTab label="Plafonske lampe" />
+        <ProductsNavTab label="Podne i zidne lampe" />
+        <ProductsNavTab label="Radne lampe" />
+        <ProductsNavTab label="Spoljna rasveta" />
+        <ProductsNavTab label="Spotovi" />
+        <ProductsNavTab label="Vintage" />
+        <ProductsNavTab label="Visilice" />
+      </Tabs>
+    </ProductsNavContainer>
+  );
 };
 
 export default ProductsNav;
--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,11 +1,33 @@
 import styled from "styled-components";
+import ProductsNav from "../../components/products-nav/ProductsNav";
+import { Typography } from "@mui/material";
 
 const ProductsContainer = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProductsTitleContainer = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-top: 16px;
+  h5 {
+    font-weight: bold;
+  }
 `;
 
 const Products = () => {
-  return <ProductsContainer>Some text products.</ProductsContainer>;
+  return (
+    <ProductsContainer>
+      <ProductsTitleContainer>
+        <Typography variant="h5">NAŠA PONUDA</Typography>
+      </ProductsTitleContainer>
+      <ProductsNav />
+      Some text products.
+    </ProductsContainer>
+  );
 };
 
 export default Products;
